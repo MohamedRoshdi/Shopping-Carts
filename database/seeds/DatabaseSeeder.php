e2ab87759cd1c56eccfa0ed3d299358ab3450cbb
MohamedRoshdi <mohamedroshdy523@gmail.com>
2017-12-25 17:24:56 -0800

    Add data to database
    add data to the database using database seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('carts')->insert([
+            'cart_name' => 'Order Cart',
+        ]);
+        DB::table('carts')->insert([
+            'cart_name' => 'Wish List Cart',
+        ]);
+
+        DB::table('products')->insert([
+            'product_name' => 'Camera',
+            'product_photo' => 'camera.jpg',
+            'product_type' => 'Normal Item',
+            'product_price' => '200',
+        ]);
+        DB::table('products')->insert([
+            'product_name' => 'Hard',
+            'product_photo' => 'external-hard-drive.jpg',
+            'product_type' => 'Sale Item',
+            'product_price' => '360',
+        ]);
+        DB::table('products')->insert([
+            'product_name' => 'Watch',
+            'product_photo' => 'watch.jpg',
+            'product_type' => 'Normal Item',
+            'product_price' => '150',
+        ]);
     }
 }
